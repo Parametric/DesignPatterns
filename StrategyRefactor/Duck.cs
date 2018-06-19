a807f9a34243908eedb9500e5f50e46be4042f34
Brent Sullivan <brntsllvn@gmail.com>
2018-06-19 14:51:58 -0700

    commit source

---
+++ StrategyRefactor/Duck.cs
@@ -0,0 +1,45 @@
+﻿namespace StrategyRefactor
+{
+    public class Duck
+    {
+        public Duck(int ageInDays, bool isBird, int utilityScore)
+        {
+            AgeInDays = ageInDays;
+            IsBird = isBird;
+            UtilityScore = utilityScore;
+        }
+
+        public int AgeInDays { get; set; }
+        public bool IsBird { get; set; }
+        public int UtilityScore { get; set; }
+
+        public string SayHello()
+        {
+            string greeting;
+            if (IsBird)
+            {
+                if (AgeInDays < 60)
+                {
+                    greeting = "tweet";
+                }
+                else
+                {
+                    greeting = "quack";
+                }
+            }
+            else
+            {
+                if (UtilityScore == 0)
+                {
+                    greeting = "squeak";
+                }
+                else
+                {
+                    greeting = "quack";
+                }
+            }
+
+            return greeting;
+        }
+    }
+}
---
+++ StrategyTest/Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using StrategyRefactor;
+
+namespace StrategyTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void YoungDuckSaysTweet()
+        {
+            var duck = new Duck(20, true, 0);
+            var greeting = duck.SayHello();
+            Assert.That(greeting, Is.EqualTo("tweet"));
+        }
+
+        [Test]
+        public void OldDuckSaysQuack()
+        {
+            var duck = new Duck(61, true, 0);
+            var greeting = duck.SayHello();
+            Assert.That(greeting, Is.EqualTo("quack"));
+        }
+
+        [Test]
+        public void LowUtilityDuckToolSaysSqueak()
+        {
+            var duck = new Duck(0, false, 0);
+            var greeting = duck.SayHello();
+            Assert.That(greeting, Is.EqualTo("squeak"));
+        }
+
+        [Test]
+        public void NonZeroUtilityDuckToolSaysQuack()
+        {
+            var duck = new Duck(0, false, 10);
+            var greeting = duck.SayHello();
+            Assert.That(greeting, Is.EqualTo("quack"));
+        }
+    }
+}
+
+
