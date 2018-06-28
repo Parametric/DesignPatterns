1da5c01e6c4ff934dec61a471df82232951fa86a
Brent Sullivan <brntsllvn@gmail.com>
2018-06-28 09:08:28 -0700

    add decorator starter code

---
+++ DecoratorRefactor/Coffee.cs
@@ -0,0 +1,55 @@
+﻿namespace DecoratorRefactor
+{
+    public interface ICoffee
+    {
+        double Cost();
+    }
+
+    public class HouseBlendPlain : ICoffee
+    {
+        public double Cost()
+        {
+            return 2.85;
+        }
+    }
+
+    public class HouseBlendWithMocha : ICoffee
+    {
+        public double Cost()
+        {
+            return 2.85 + 0.35;
+        }
+    }
+
+    public class HouseBlendWithMilk : ICoffee
+    {
+        public double Cost()
+        {
+            return 2.85 + 0.25;
+        }
+    }
+
+    public class DripCoffeePlain : ICoffee
+    {
+        public double Cost()
+        {
+            return 3.15;
+        }
+    }
+
+    public class DripCoffeeWithMocha : ICoffee
+    {
+        public double Cost()
+        {
+            return 3.15 + 0.35;
+        }
+    }
+
+    public class DripCoffeeWithMilk : ICoffee
+    {
+        public double Cost()
+        {
+            return 3.15 + 0.25;
+        }
+    }
+}
---
+++ DecoratorTest/Tests.cs
@@ -0,0 +1,50 @@
+﻿using DecoratorRefactor;
+using NUnit.Framework;
+
+namespace DecoratorTest
+{
+    public class Tests
+    {
+        [Test]
+        public void HouseBlendPlainCostIsRight()
+        {
+            var coffee = new HouseBlendPlain();
+            Assert.That(coffee.Cost(), Is.EqualTo(2.85));
+        }
+
+        [Test]
+        public void HouseBlendWithMochaCostIsRight()
+        {
+            var coffee = new HouseBlendWithMocha();
+            Assert.That(coffee.Cost(), Is.EqualTo(2.85 + 0.35));
+        }
+
+        [Test]
+        public void HouseBlendWithMilkCostIsRight()
+        {
+            var coffee = new HouseBlendWithMilk();
+            Assert.That(coffee.Cost(), Is.EqualTo(2.85 + 0.25));
+        }
+
+        [Test]
+        public void DripCoffeePlainCostIsRight()
+        {
+            var coffee = new DripCoffeePlain();
+            Assert.That(coffee.Cost(), Is.EqualTo(3.15));
+        }
+
+        [Test]
+        public void DripCoffeeWithMochaCostIsRight()
+        {
+            var coffee = new DripCoffeeWithMocha();
+            Assert.That(coffee.Cost(), Is.EqualTo(3.15 + 0.35));
+        }
+
+        [Test]
+        public void DripCoffeeWithMilkCostIsRight()
+        {
+            var coffee = new DripCoffeeWithMilk();
+            Assert.That(coffee.Cost(), Is.EqualTo(3.15 + 0.25));
+        }
+    }
+}
