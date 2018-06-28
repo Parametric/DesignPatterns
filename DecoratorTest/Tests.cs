ee956fff6adbee639014f4165192cf369e6bffd2
Brent Sullivan <brntsllvn@gmail.com>
2018-06-28 09:49:58 -0700

    don't use type inference

--- DecoratorTest/Tests.cs
+++ DecoratorTest/Tests.cs
@@ -8,42 +8,42 @@ namespace DecoratorTest
         [Test]
         public void HouseBlendPlainCostIsRight()
         {
-            var coffee = new HouseBlendPlain();
+            ICoffee coffee = new HouseBlendPlain();
             Assert.That(coffee.Cost(), Is.EqualTo(2.85));
         }
 
         [Test]
         public void HouseBlendWithMochaCostIsRight()
         {
-            var coffee = new HouseBlendWithMocha();
+            ICoffee coffee = new HouseBlendWithMocha();
             Assert.That(coffee.Cost(), Is.EqualTo(2.85 + 0.35));
         }
 
         [Test]
         public void HouseBlendWithMilkCostIsRight()
         {
-            var coffee = new HouseBlendWithMilk();
+            ICoffee coffee = new HouseBlendWithMilk();
             Assert.That(coffee.Cost(), Is.EqualTo(2.85 + 0.25));
         }
 
         [Test]
         public void DripCoffeePlainCostIsRight()
         {
-            var coffee = new DripCoffeePlain();
+            ICoffee coffee = new DripCoffeePlain();
             Assert.That(coffee.Cost(), Is.EqualTo(3.15));
         }
 
         [Test]
         public void DripCoffeeWithMochaCostIsRight()
         {
-            var coffee = new DripCoffeeWithMocha();
+            ICoffee coffee = new DripCoffeeWithMocha();
             Assert.That(coffee.Cost(), Is.EqualTo(3.15 + 0.35));
         }
 
         [Test]
         public void DripCoffeeWithMilkCostIsRight()
         {
-            var coffee = new DripCoffeeWithMilk();
+            ICoffee coffee = new DripCoffeeWithMilk();
             Assert.That(coffee.Cost(), Is.EqualTo(3.15 + 0.25));
         }
     }
