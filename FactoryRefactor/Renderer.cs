6be27c6f84f03a1f2b24918d59cffc452456de75
Brent Sullivan <brntsllvn@gmail.com>
2018-07-13 12:08:17 -0700

    final abstract factory code

--- FactoryRefactor/Renderer.cs
+++ FactoryRefactor/Renderer.cs
@@ -1,31 +1,104 @@
 ﻿namespace FactoryRefactor
 {
-    public class Renderer
+    public interface IWidgetFactory
     {
-        public string Draw(string operatingSystem)
+        IWidget CreateButton();
+        IWidget CreateMenu();
+    }
+
+    public class FlatLinuxFactory : IWidgetFactory
+    {
+        public IWidget CreateButton()
         {
-            IWidget button;
-            if (operatingSystem.Equals("Linux"))
-            {
-                button = new LinuxButton();
-            }
-            else {
-                button = new WindowsButton();
-            }
+            return new FlatLinuxButton();
+        }
 
-            IWidget menu;
-            if (button.GetType() == typeof(LinuxButton))
-            {
-                menu = new LinuxMenu();
-            }
-            else
-            {
-                menu = new WindowsMenu();
-            }
-
-            return button.Draw() + " & " + menu.Draw();
+        public IWidget CreateMenu()
+        {
+            return new FlatLinuxMenu();
         }
     }
+    public class WindowsFactory : IWidgetFactory
+    {
+        public IWidget CreateButton()
+        {
+            return new WindowsButton();
+        }
+
+        public IWidget CreateMenu()
+        {
+            return new WindowsMenu();
+        }
+    }
+    public class ChromeOsFactory : IWidgetFactory
+    {
+        public IWidget CreateButton()
+        {
+            return new ChromeOsButton();
+        }
+
+        public IWidget CreateMenu()
+        {
+            return new ChromeOsMenu();
+        }
+    }
+    public class AndroidFactory : IWidgetFactory
+    {
+        public IWidget CreateButton()
+        {
+            return new AndroidButton();
+        }
+
+        public IWidget CreateMenu()
+        {
+            return new AndroidMenu();
+        }
+    }
+
+    public class Renderer
+    {
+        public string Draw(IWidgetFactory factory)
+        {
+            IWidget button = factory.CreateButton();
+            IWidget menu = factory.CreateMenu();
+            return button.Draw() + " & " + menu.Draw();
+        }
+
+        //public string Draw(string operatingSystem)
+        //{
+        //    IWidget button;
+        //    if (operatingSystem.Equals("FlatLinux"))
+        //    {
+        //        button = new FlatLinuxButton();
+        //    }
+        //    else if (operatingSystem.Equals("Windows")) 
+        //    {
+        //        button = new WindowsButton();
+        //    }
+        //    else
+        //    {
+        //        button = new ChromeOsButton();
+        //    }
+
+        //    IWidget menu;
+        //    if (button.GetType() == typeof(FlatLinuxButton))
+        //    {
+        //        menu = new FlatLinuxMenu();
+        //    }
+        //    else if (button.GetType() == typeof(WindowsButton))
+        //    {
+        //        menu = new WindowsMenu();
+        //    }
+        //    else
+        //    {
+        //        menu = new ChromeOsMenu();
+        //    }
+
+        //    return button.Draw() + " & " + menu.Draw();
+        //}
+    }
+
+
 }
 public interface IWidget
 {
@@ -46,6 +119,13 @@ public class WindowsButton : IWidget
         return "Windows button";
     }
 }
+public class FlatLinuxButton : IWidget
+{
+    public string Draw()
+    {
+        return "FlatLinux button";
+    }
+}
 
 public class LinuxMenu : IWidget
 {
@@ -60,5 +140,45 @@ public class WindowsMenu : IWidget
     public string Draw()
     {
         return "Windows menu";
+    }
+}
+
+public class FlatLinuxMenu : IWidget
+{
+    public string Draw()
+    {
+        return "FlatLinux menu";
+    }
+}
+
+public class ChromeOsButton : IWidget
+{
+    public string Draw()
+    {
+        return "ChromeOS button";
+    }
+}
+public class ChromeOsMenu : IWidget
+{
+    public string Draw()
+    {
+        return "ChromeOS menu";
+    }
+}
+
+
+public class AndroidButton : IWidget
+{
+    public string Draw()
+    {
+        return "Android button";
+    }
+}
+
+public class AndroidMenu : IWidget
+{
+    public string Draw()
+    {
+        return "Android menu";
     }
 }
--- FactoryTest/Tests.cs
+++ FactoryTest/Tests.cs
@@ -6,20 +6,50 @@ namespace FactoryTest
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void RendersLinuxCorrectly()
-        {
-            var renderer = new Renderer();
-            var result = renderer.Draw("Linux");
-            Assert.That(result, Is.EqualTo("Linux button & Linux menu"));
-        }
+        //[Test]
+        //public void RendersLinuxCorrectly()
+        //{
+        //    var renderer = new Renderer();
+        //    var result = renderer.Draw("Linux");
+        //    Assert.That(result, 
+        //        Is.EqualTo("Linux button & Linux menu"));
+        //}
 
         [Test]
         public void RendersWindowsCorrectly()
         {
             var renderer = new Renderer();
-            var result = renderer.Draw("Windows");
-            Assert.That(result, Is.EqualTo("Windows button & Windows menu"));
+            var result = renderer.Draw(new WindowsFactory());
+            Assert.That(result, 
+                Is.EqualTo("Windows button & Windows menu"));
         }
+
+        [Test]
+        public void RendersFlatLinuxCorrectly()
+        {
+            var renderer = new Renderer();
+            var result = renderer.Draw(new FlatLinuxFactory());
+            Assert.That(result,
+                Is.EqualTo("FlatLinux button & FlatLinux menu"));
+        }
+
+        [Test]
+        public void RendersChromeOsCorrectly()
+        {
+            var renderer = new Renderer();
+            var result = renderer.Draw(new ChromeOsFactory());
+            Assert.That(result,
+                Is.EqualTo("ChromeOS button & ChromeOS menu"));
+        }
+
+        [Test]
+        public void RenderAndroidCorrectly()
+        {
+            var renderer = new Renderer();
+            var result = renderer.Draw(new AndroidFactory());
+            Assert.That(result,
+                Is.EqualTo("Android button & Android menu"));
+        }
+
     }
 }
